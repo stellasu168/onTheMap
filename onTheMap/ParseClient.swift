d4ff6fbe2ffafd273a15f194a8c8b1193fa760e3
stellasu168 <millionstars@gmail.com>
2015-12-03 18:56:58 -0800

    Delete taskForPutMethod

--- onTheMap/ParseClient.swift
+++ onTheMap/ParseClient.swift
@@ -93,55 +93,9 @@ class ParseClient: NSObject {
         
         return task
     }
-/*
-    // MARK: PUT
-    func taskForPutMethod(method: String, jsonBody: [String : AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) -> NSURLSessionDataTask {
-        
-        // Build the URL and configure the request
-        let urlString = ParseClient.Constants.BaseURLSecure + method
-        let url = NSURL(string: urlString)!
-        let request = NSMutableURLRequest(URL: url)
-        
-        var jsonifyError: NSError? = nil
-        
-        request.HTTPMethod = "PUT"
-        request.addValue(Constants.ParseAppID, forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue(Constants.RestAPIKey, forHTTPHeaderField: "X-Parse-REST-API-Key")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        do {
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(jsonBody, options: [])
-        } catch let error as NSError {
-            jsonifyError = error
-            print(jsonifyError)
-            request.HTTPBody = nil
-        }
-        
-
-        // Make the request
-        let task = session.dataTaskWithRequest(request) { data, response, downloadError in
-            
-            // Parse and use the data (happens in completion handler)
-            if let error = downloadError {
-                let newError = ParseClient.errorForData(data, response: response, error: error)
-                completionHandler(result: nil, error: newError)
-                print("taskForPutMethod - \(error.localizedDescription)")
-                
-            }
-            else {
-                ParseClient.parseJSONWithCompletionHandler(data!, completionHandler: completionHandler)
-            }
-        }
-        
-        // Start the request
-        task.resume()
-        
-        return task
-    }
-    
-*/
     // MARK: Helpers
     
-    // Helper: Given a response with error, see if a status_message is returned, otherwise return the previous error
+    // Given a response with error, see if a status_message is returned, otherwise return the previous error
     class func errorForData(data: NSData?, response: NSURLResponse?, error: NSError?) -> NSError {
      
         if data == nil {
@@ -164,7 +118,7 @@ class ParseClient: NSObject {
         return error!
     }
     
-    // Helper: Given raw JSON, return a useable Foundation object
+    // Given raw JSON, return a useable Foundation object
     class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
         
         var parsingError: NSError? = nil
@@ -193,7 +147,7 @@ class ParseClient: NSObject {
         }
     }
     
-    // Helper function: Given a dictionary of parameters, convert to a string for a url
+    // Given a dictionary of parameters, convert to a string for a url
     class func escapedParameters(parameters: [String : AnyObject]) -> String {
         
         let queryItems = parameters.map { NSURLQueryItem(name: $0, value: $1 as? String) }
